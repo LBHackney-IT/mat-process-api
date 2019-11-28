098940b3f7840444c935db93b6f085e13d74c14c
Liudvikas Taluntis <liudvikast@gmail.com>
2019-11-28 16:46:59 +0000

    Added a method overload for creating MatProcessData Domain object
    -----Implementation------
    Had to extract dateOfCreation out, since I believe that the dates DateCreated and DateLastModified have to be equal. The alternative is them differing by miliseconds, which might or might not cause issues in the future - who knows how it's going to be used.
    
    The process stage... I left it as a string because I was told that it's still undecided on its format. Therefore I don't feel like changing a lot of existing code around that for an uncertainty.
    
    DateCompleted - a minimum value. Not elegant, but it was decided that it's better than having a nullable DateTime type.
    
    Why not an instance method? Was following the previous method in the factory. Don't understand enough C# to question that decision yet. I guess, it's all good as long as this method doesn't hold any state or anything.
    
    ------Tests--------
    Using reflections because the regular comparison that's commented out returns a nonsensical error saying that it expected <{ }>, but instead got <{ }>.
    
    It might have been simply comparing object references, but who knows.
    
    So instead I check how many fields the anonymous object has. It's supposed to be zero, since the requirement is that it has to be set as empty object.

--- mat-process-api/V1/Factories/ProcessDataFactory.cs
+++ mat-process-api/V1/Factories/ProcessDataFactory.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using mat_process_api.V1.Domain;
+using mat_process_api.V1.Boundary;
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization;
 
@@ -21,6 +22,25 @@ namespace mat_process_api.V1.Factories
                 //return empty object, rather than null, as front end will be expecting emtpy object?
                 return new MatProcessData();
             }
+        }
+
+        public static MatProcessData CreateProcessDataObject(PostInitialProcessDocumentRequest requestObject) //Maps PostInitialDocumentRequest object to ProcessData domain object
+        {
+            DateTime dateOfCreation = DateTime.Now; // need this here because DateCreated and DateLastModified have to be equal
+
+            return new MatProcessData()
+            {
+                Id = requestObject.processRef,
+                ProcessType = requestObject.processType,
+                DateCreated = dateOfCreation,
+                DateLastModified = dateOfCreation,
+                DateCompleted = DateTime.MinValue,
+                ProcessDataSchemaVersion = requestObject.processDataSchemaVersion,
+                ProcessStage = "0",
+                PreProcessData = new { },
+                ProcessData = new { },
+                PostProcessData = new { }
+            };
         }
     }
 }
