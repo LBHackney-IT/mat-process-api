db00e6feaa036d1db47880bcb1260b1058e0a88e
Liudvikas Taluntis <liudvikast@gmail.com>
2020-01-07 15:11:02 +0000

    Added Exception Handling unit tests for the PostInitDoc controller method.
    Difference between these and the acceptance ones is that these ones use mocks, rahter than actual instances.

--- mat-process-api.Tests/V1/Controllers/ProcessDataControllerTests.cs
+++ mat-process-api.Tests/V1/Controllers/ProcessDataControllerTests.cs
@@ -10,6 +10,7 @@ using mat_process_api.V1.Boundary;
 using mat_process_api.V1.Controllers;
 using mat_process_api.V1.Domain;
 using mat_process_api.V1.UseCase;
+using mat_process_api.V1.Gateways;
 using mat_process_api.V1.Validators;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
@@ -110,6 +111,69 @@ namespace mat_process_api.Tests.V1.Controllers
         }
 
         [Test]
+        public void given_postInitialProcessDocument_controller_method_call_when_request_validation_fails_then_the_controller_returns_correct_badRequest_response()
+        {
+            //arrange
+            int errorCount = faker.Random.Int(1,10); //simulate from 1 to 10 validation errors (triangulation).
+            var validationErrorList = new List<ValidationFailure>(); //this list will be used as constructor argument for 'ValidationResult'.
+            for (int i = errorCount; i > 0; i--) { validationErrorList.Add(new ValidationFailure(faker.Random.Word(), faker.Random.Word())); } //generate from 1 to 10 fake validation errors. Single line for-loop so that it wouldn't distract from what's key in this test.
+
+            var fakeValidationResult = new FV.ValidationResult(validationErrorList); //Need to create ValidationResult so that I could setup Validator mock to return it upon '.Validate()' call. Also this is the only place where it's possible to manipulate the validation result - You can only make the validation result invalid by inserting a list of validation errors as a parameter through a constructor. The boolean '.IsValid' comes from expression 'IsValid => Errors.Count == 0;', so it can't be set manually.
+            _mockValidator.Setup(v => v.Validate(It.IsAny<PostInitialProcessDocumentRequest>())).Returns(fakeValidationResult);
+
+            //act
+            var controllerResponse = _processDataController.PostInitialProcessDocument(new PostInitialProcessDocumentRequest());
+            var result = (ObjectResult)controllerResponse;
+            var resultContents = (IList<ValidationFailure>)result.Value;
+
+            //assert
+            Assert.NotNull(controllerResponse);
+            Assert.IsInstanceOf<BadRequestObjectResult>(result); // should be bad request, since validation fails.
+            Assert.NotNull(result);
+            Assert.IsInstanceOf<IList<ValidationFailure>>(resultContents);
+            Assert.NotNull(resultContents);
+
+            Assert.AreEqual(errorCount, resultContents.Count); // there should be 1 validation failure coming from the invalid guid
+            Assert.AreEqual(400, result.StatusCode); // expecting bad request result
+        }
+
+        [Test]
+        public void given_request_with_unique_key_that_already_exists_when_postInitialProcessDocument_controller_method_is_called_then_upon_catching_custom_conflict_exception_the_correct_conflict_result_is_returned() //testing exception handling. in this case testing how our custom exception bubbles up from gateway. chose conflict exception to test 2 things at once - exception handling + custom exception wrapping.
+        {
+            //arrange
+            //setup mocks:
+            _mockValidator.Setup(v => v.Validate(It.IsAny<PostInitialProcessDocumentRequest>())).Returns(new FV.ValidationResult()); //set up mock validation to return Validation with no errors.
+            _mockUsecase.Setup(g => g.ExecutePost(It.IsAny<PostInitialProcessDocumentRequest>())).Returns(() => throw new ConflictException(faker.Random.Word(), new Exception(faker.Random.Word())));
+
+            //setup double insert:
+            string conflictGuid = faker.Random.Guid().ToString(); //This is the conflicting Guid, that will be repeated accross 2 request objects.
+
+            PostInitialProcessDocumentRequest requestObject1 = MatProcessDataHelper.CreatePostInitialProcessDocumentRequestObject();
+            requestObject1.processRef = conflictGuid; // set conflict Guid.
+            _processDataController.PostInitialProcessDocument(requestObject1); //The process document with conflict Guid identifier has been inserted into the database.
+
+            PostInitialProcessDocumentRequest requestObject2 = MatProcessDataHelper.CreatePostInitialProcessDocumentRequestObject();
+            requestObject2.processRef = conflictGuid; // set conflict Guid.
+
+            //act
+            var controllerResponse = _processDataController.PostInitialProcessDocument(requestObject2); //The attempt to insert another document into the database with the already existing identifier (conflict guid). Conflict happens.
+            var actualResult = (ObjectResult)controllerResponse;
+            var errorMessage = (string)actualResult.Value;
+
+            //assert
+            Assert.NotNull(controllerResponse);
+            Assert.IsInstanceOf<ConflictObjectResult>(actualResult);
+            Assert.NotNull(actualResult);
+            Assert.IsInstanceOf<string>(errorMessage);
+            Assert.NotNull(errorMessage);
+            Assert.IsNotEmpty(errorMessage);
+
+            Assert.AreEqual(409, actualResult.StatusCode);
+            Assert.AreNotEqual("An error inserting an object with duplicate key has occured - ", errorMessage); //If they're equal, then it means that empty exception has been appended to this sentense.
+            //There is no good way to check the error message, since it would require to cause the exception manually through writing error handling implementation in the test.
+        }
+
+        [Test]
         public void given_a_postInitialProcessDocumentRequest_when_postInitialProcessDocument_controller_method_is_called_it_then_calls_the_use_case_while_passing_the_request_object_to_it()
         {
             //arrange
@@ -171,8 +235,6 @@ namespace mat_process_api.Tests.V1.Controllers
             Assert.AreEqual(expectedResponse.Request.processRef, resultContent.Request.processRef);
             Assert.AreEqual(JsonConvert.SerializeObject(expectedResponse), JsonConvert.SerializeObject(resultContent));
         }
-
-        //There should be test for given invalid request... essentially invalid GUID, but it should be done after the validation is done.
 
         #endregion
     }
