c41689e909f1c633319d7eed7cbf6c4328a51af8
Liudvikas Taluntis <liudvikast@gmail.com>
2020-01-13 17:38:09 +0000

    Made the skeleton Post Process Image Controller.
    Paired up with Ana Bebic - was showing the ways of how API is structured.
    
    We were working on creating an endpoint to store images and do something else that's still not exactly clear due to the planing session not being done yet.
    
    Added the Controller of the POST image endpoint as well as a Boundary object and Usecase interface, which was necessary to write the tests.
    The controller is mostly done, only need to add a call to validation and some error handling.
    
    It was decided that, at least, for the time being the endpoint will not be returning anything, hence the NoContent 204.
    
    Reasoning is that the image data will passed around using base64 string, which we expect to be massive. Having a response which returns the request (like typical) is out of the question, since encoded image string is sort of useless and would take quite some network resources to pass around. Same reason we don't return the inserted image itself. So there's nothing to return.
    
    That's not exactly true, there still is an option to return a link to the image and maybe the imageId based on the implementation of the gateway we decide.
    
    The path of the new is still undecided, for the time being temporary one is set.

---
+++ mat-process-api.Tests/V1/Controllers/ProcessDataImagesControllerTests.cs
@@ -0,0 +1,80 @@
+using mat_process_api.V1.Boundary;
+using mat_process_api.V1.Controllers;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Moq;
+using System.Threading.Tasks;
+using mat_process_api.V1.UseCase;
+using Bogus;
+using Microsoft.AspNetCore.Mvc;
+
+namespace mat_process_api.Tests.V1.Controllers
+{
+    [TestFixture]
+    public class ProcessDataImagesControllerTests
+    {
+        private ProcessImageController _processImageController;
+        private Mock<IProcessImageUseCase> _mockUsecase;
+        private Faker _faker = new Faker();
+
+        [SetUp]
+        public void SetUp()
+        {
+            _mockUsecase = new Mock<IProcessImageUseCase>();
+            _processImageController = new ProcessImageController(_mockUsecase.Object);
+        }
+
+        [Test]
+        public void given_valid_request_when_postProcessImage_controller_method_is_called_then_usecase_is_called()
+        {
+            //arrange
+            var request = new PostProcessImageRequest();
+
+            //act
+            _processImageController.PostProcessImage(request);
+
+            //assert
+            _mockUsecase.Verify(u => u.ExecutePost(It.IsAny<PostProcessImageRequest>()), Times.Once);
+        }
+
+        [Test]
+        public void given_valid_request_when_postProcessImage_controller_method_is_called_then_usecase_is_called_with_correct_data()
+        {
+            //arange
+            var request = new PostProcessImageRequest()
+            {
+                processRef = _faker.Random.Guid().ToString(),
+                imageId = _faker.Random.Guid().ToString(),
+                base64Image = _faker.Random.Guid().ToString()
+            };
+
+            //act
+            _processImageController.PostProcessImage(request);
+
+            //assert
+            _mockUsecase.Verify(u => u.ExecutePost(It.Is<PostProcessImageRequest>(obj =>
+                obj.processRef == request.processRef &&
+                obj.imageId == request.imageId &&
+                obj.base64Image == request.base64Image
+                )), Times.Once);
+        }
+
+        [Test]
+        public void given_valid_request_when_postProcessImage_controller_method_is_called_then_it_returns_204_NoContent_result()
+        {
+            //arrange
+            var expectedStatusCode = 204;
+            var request = new PostProcessImageRequest();
+
+            //act
+            var controllerResponse = _processImageController.PostProcessImage(request);
+            var result = (StatusCodeResult)controllerResponse; //not 'ObjectResult' because there's no object contained in this response
+
+            //assert
+            Assert.IsInstanceOf<NoContentResult>(result);
+            Assert.AreEqual(expectedStatusCode, result.StatusCode);
+        }
+    }
+}
---
+++ mat-process-api/V1/Controllers/ProcessImageController.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using mat_process_api.V1.Boundary;
+using mat_process_api.V1.UseCase;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace mat_process_api.V1.Controllers
+{
+    [ApiVersion("1.0")]
+    [Route("api/v1/processImageData")] //<----- do we change this? url undecided
+    [ApiController]
+    [Produces("application/json")]
+    public class ProcessImageController : Controller
+    {
+        private IProcessImageUseCase _processImageUseCase;
+
+        public ProcessImageController(IProcessImageUseCase usecase)
+        {
+            _processImageUseCase = usecase;
+        }
+
+        /// <summary>
+        /// TODO: Add description
+        /// </summary>
+        /// <param name="imageData"></param>
+        [HttpPost]
+        [Produces("application/json")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        public IActionResult PostProcessImage([FromBody] PostProcessImageRequest imageData)
+        {
+            _processImageUseCase.ExecutePost(imageData);
+
+            return NoContent();
+        }
+    }
+}
