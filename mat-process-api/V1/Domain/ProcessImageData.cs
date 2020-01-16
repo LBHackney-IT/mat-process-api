dc360809460c919a9f87a9921306798322fb1f93
Liudvikas Taluntis <liudvikast@gmail.com>
2020-01-16 14:27:52 +0000

    Modified the ProcessImageData domain object to meet new Spec. Added Tests for it.
    Had to change the domain object (for POST endpoint) because there was a change of what's expected to be passed into the gateway. This time added tests for it as it's only know that it's structure is clear.

--- mat-process-api/V1/Domain/ProcessImageData.cs
+++ mat-process-api/V1/Domain/ProcessImageData.cs
@@ -5,6 +5,15 @@ namespace mat_process_api.V1.Domain
     {
         public string processRef { get; set; }
         public string imageId { get; set; }
+
+        public Base64DecodedData imageData { get; set; }
+        
+    }
+
+    public class Base64DecodedData
+    {
         public byte[] imageBytes { get; set; }
+        public string imageType { get; set; }
+        public string imageExtension { get; set; }
     }
 }
