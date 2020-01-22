da8769c24a2024d3fa76c6454ea42d8dd3866d5c
Liudvikas Taluntis <liudvikast@gmail.com>
2020-01-22 11:45:27 +0000

    Modified the GetProcessImageRequest Validator.
    Added 2 extra tests for the 2 new fields of GetProcessImageRequest object ("processType, fileExtension). These tests are about throwing an error when a blank parameter is supplied. Added implementaion to validator based of these tests.
    
    Also added back the removed tests and validation for the "processRef" and "imageId" fields. The ones that did the not null, not empty checks. Reason for it is that even though request with such null values wouldn't pass the validation, it wouldn't give the error message specific enough to what the issue is. I feel that providing 'null' argument versus an invalid one should return different error messages.
    
    Now, there was an argument raised on the PR against these checks saying that: since now the parameters are being passed in through url path, when you don't provide them, it will simply be interpreted as a different path - hence 404 path not found.
    However, in response to that, I'd say: if you had provided a url encoded whitespace (which sometimes gets encoded automatically) as an argument, it would count as a valid path, and the  .NET would interpret the whitespace as 'null'. I checked it through the browser and debugger - it confirms it.
    
    Example: "{insert your host}/api/v1/processImageData/%20/a68e234a-bd08-3542-4b89-42572b6adc65/e68e234a-bd08-3542-4b89-42572b6adc65/%20"

--- mat-process-api/V1/Validators/GetProcessImageRequestValidator.cs
+++ mat-process-api/V1/Validators/GetProcessImageRequestValidator.cs
@@ -13,10 +13,12 @@ namespace mat_process_api.V1.Validators
         {
             ValidatorOptions.CascadeMode = CascadeMode.StopOnFirstFailure; // Stops the validator from going into DependentRules or deeper into any single rule in general. This is needed because it doesn't make sense to keep checking if property is empty, once it was determined it's null.
 
-            RuleFor(req => req.processRef).Must(ValidateGuid)
-               .WithMessage("You need to provide a valid process reference.");
-            RuleFor(req => req.imageId).Must(ValidateGuid)
-                .WithMessage("You need to provide a valid Image Id.");
+            RuleFor(req => req.processRef).NotNull().WithMessage("Process reference must be provided.").NotEmpty().WithMessage("Process reference must be provided.") //Added the NotNull,NotEmpty checks back to get more specific error messages
+                .Must(ValidateGuid).WithMessage("You need to provide a valid process reference.");
+            RuleFor(req => req.imageId).NotNull().WithMessage("Image Id must be provided.").NotEmpty().WithMessage("Image Id must be provided.")
+                .Must(ValidateGuid).WithMessage("You need to provide a valid Image Id.");
+            RuleFor(req => req.processType).NotNull().WithMessage("Process Type must be provided.").NotEmpty().WithMessage("Process Type must be provided.");
+            RuleFor(req => req.fileExtension).NotNull().WithMessage("File Extension must be provided.").NotEmpty().WithMessage("File Extension must be provided.");
         }
 
         private bool ValidateGuid(string guid)
