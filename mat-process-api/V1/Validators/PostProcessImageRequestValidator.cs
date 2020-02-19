e681f0f32293159e389eab6cae5317f3849ab4ac
LBHTKarki <58432863+LBHTKarki@users.noreply.github.com>
2020-02-19 13:47:26 +0000

    version for dev testing

--- mat-process-api/V1/Validators/PostProcessImageRequestValidator.cs
+++ mat-process-api/V1/Validators/PostProcessImageRequestValidator.cs
@@ -15,9 +15,9 @@ namespace mat_process_api.V1.Validators
                .WithMessage("You need to provide a valid process reference.");
             RuleFor(req => req.imageId).NotNull().WithMessage("Image Id must be provided.").NotEmpty().WithMessage("Image Id must be provided.").Must(ValidateGuid)
                 .WithMessage("You need to provide a valid Image Id.");
-            RuleFor(req => req.base64Image).NotNull().WithMessage("Base64 Image string must be provided.").NotEmpty().WithMessage("Base64 Image string must be provided.")
-                .Must(ValidateBase64Length).WithMessage("You need to provide a valid Base64 Image string.")
-                .Matches(new Regex(@"^data:image\/([^_\W]{3,});base64,([^_\W]|[+\/])+={0,3}$")).WithMessage("You need to provide a valid Base64 Image string.");
+            RuleFor(req => req.base64Image).NotNull().WithMessage("Base64 Image string must be provided.").NotEmpty().WithMessage("Base64 Image string must be provided.");
+                //.Must(ValidateBase64Length).WithMessage("You need to provide a valid Base64 Image string.")
+                //.Matches(new Regex(@"^data:image\/([^_\W]{3,});base64,([^_\W]|[+\/])+={0,3}$")).WithMessage("You need to provide a valid Base64 Image string.");
         }
 
         private bool ValidateGuid(string guid)
